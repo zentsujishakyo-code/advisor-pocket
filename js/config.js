f0babaf1a17af72b563b03e9a151c2c6ac4bd9ac
zentsujishakyo-code <zentsuji.shakyo@gmail.com>
2026-05-21 18:57:12 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   GAS_URL: 'https://script.google.com/macros/s/AKfycbw6LC-0u4Bu0xELgbnA83FLWBxl4KMMDsVVMvRACCZvdBitDREqkmdHS7hlZj5HPGS0Mg/exec',
   
   // アプリのバージョン
-  VERSION: '0.5.0',
+  VERSION: '0.6.0',
   
   // 開発モード (true: コンソールにデバッグ出力)
   DEBUG: false,
